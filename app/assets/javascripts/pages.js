9f6ed136067b94209124cce0dd717d72936e8dbb
Petermuturi <pmuturi65@gmail.com>
2016-10-28 21:28:36 +0300

    added predict button

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,11 +1,8 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
 // Nav hamburger menu
 $(document).ready(function(){
   $(function() {
     $(".menu").click(function() {
-      $(".right-nav").toggleClass("show-nav", 1000, "easeOutSine");
+      $(".right-nav").toggleClass("show-nav", 500, "easeOutSine");
     });
   });
 });
