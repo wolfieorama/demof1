c3af9f84129caae96e922a300c90bdee3eba060d
Petermuturi <pmuturi65@gmail.com>
2016-10-28 19:24:46 +0300

    added scroll js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,9 +3,44 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 // Nav hamburger menu
 $(document).ready(()=>{
-  $(function() {
+  $(()=> {
     $(".menu").click(()=> {
       $(".right-nav").toggleClass("show-nav", 1000, "easeOutSine");
     });
   });
 })
+// Back to top!!
+$(document).ready(()=> {
+  function checkPosition() {
+    if ($(this).scrollTop() > 200) {
+      $('.go-top').fadeIn(500);
+    } else {
+      $('.go-top').fadeOut(300);
+    }
+  }
+  // Show or hide the sticky footer button
+  $(window).scroll(checkPosition);
+
+  // Animate the scroll to top
+  $('.go-top').click((event)=> {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 1000);
+  });
+  checkPosition();
+});
+
+// scroll down with ease!!
+   $(function() {
+ $('a[href*=#]:not([href=#])').click(()=> {
+   if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+     let target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html,body').animate({
+         scrollTop: target.offset().top
+       }, 1000);
+       return false;
+     }
+   }
+ });
+});
