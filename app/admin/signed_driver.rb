c1a9f2e98947fff89494638cf5f955535ccf6634
wolfieorama <wachirawamunyi@gmail.com>
2016-10-28 15:43:22 +0300

    added rails 2 select in active admin and front

--- app/admin/signed_driver.rb
+++ app/admin/signed_driver.rb
@@ -1,4 +1,21 @@
 ActiveAdmin.register SignedDriver do
   permit_params :team_id, :driver_id
 
+  index do
+    column "Driver", :driver_id do |did|
+      did.driver.driver_name
+    end
+    column "Team", :team_id do |tid|
+      tid.team.team_name
+    end
+    actions
+  end
+
+  form do |f|
+    f.input :team, as: :select2
+
+    f.input :driver, as: :select2
+    actions
+  end
+
 end
--- app/assets/javascripts/predictions.js
+++ app/assets/javascripts/predictions.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+  $("#predictions_first").select2({
+    theme: "classic"
+  });
+  $("#predictions_second").select2({
+    theme: "classic"
+  });
+  $("#predictions_third").select2({
+    theme: "classic"
+  });
+});
