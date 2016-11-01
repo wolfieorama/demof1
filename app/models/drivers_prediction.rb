0a44ca46df98e5151f1ce6219495eb79e04428ba
wolfieorama <wachirawamunyi@gmail.com>
2016-11-01 12:12:32 +0300

    cleaning out modelling

---
+++ app/models/drivers_prediction.rb
@@ -0,0 +1,2 @@
+class DriversPrediction < ActiveRecord::Base
+end
--- spec/models/drivers_prediction_spec.rb
+++ spec/models/drivers_prediction_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Prediction, type: :model do
+RSpec.describe DriversPrediction, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
